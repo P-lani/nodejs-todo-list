bf5bd6a2dcd3459213a761794f0094dd792f5c91
P-lani <dndtkd2@naver.com>
2024-09-03 15:14:13 +0900

    백엔드 코드 수정 완료

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // app.js
 
 import express from "express";
-import connect from "./schemas/index.js";
-import todosRouter from "./routes/todos.router.js";
-import errorHandlerMiddleware from "./middlewares/error-handler.middleware.js";
+import connect from './schemas/index.js';
+import todosRouter from './routes/todos.router.js';
+import errorHandlerMiddleware from './middlewares/error-handler.middleware.js';
 
 const app = express();
 const PORT = 3000;
@@ -15,22 +15,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // static Middleware, express.static()을 사용하여 정적 파일을 제공합니다.
-app.use(express.static("./assets"));
+app.use(express.static('./assets'));
 
 app.use((req, res, next) => {
-     console.log("Request URL:", req.originalUrl, " - ", new Date());
+     console.log('Request URL:', req.originalUrl, ' - ', new Date());
      next();
 });
 const router = express.Router();
 
-router.get("/", (req, res) => {
-     return res.json({ message: "Hi!" });
+router.get('/', (req, res) => {
+     return res.json({ message: 'Hi!' });
 });
 
-app.use("/api", [router, todosRouter]);
+app.use('/api', [router, todosRouter]);
 
 app.use(errorHandlerMiddleware);
 
 app.listen(PORT, () => {
-     console.log(PORT, "포트로 서버가 열렸어요!");
+     console.log(PORT, '포트로 서버가 열렸어요!');
 });
